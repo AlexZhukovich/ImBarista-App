4985cca31845cd04a0e8f1f961cf0eef2451f0c2
Alex Zhukovich <zhukovich.a@gmail.com>
2019-06-17 21:54:48 +0200

    Added google-material-design-components to build.gradle file; Added SignInActivity

---
+++ app/src/main/java/com/alexzh/imbarista/signin/SignInActivity.kt
@@ -0,0 +1,13 @@
+package com.alexzh.imbarista.signin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.alexzh.imbarista.R
+
+class SignInActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
