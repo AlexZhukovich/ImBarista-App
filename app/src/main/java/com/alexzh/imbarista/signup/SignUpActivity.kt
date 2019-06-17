29c052bb8071541feab6368073ad445664111e3b
Alex Zhukovich <zhukovich.a@gmail.com>
2019-06-17 22:12:05 +0200

    Added possibility to launch SignUpActivity from the SignInActivity

--- app/src/main/java/com/alexzh/imbarista/signup/SignUpActivity.kt
+++ app/src/main/java/com/alexzh/imbarista/signup/SignUpActivity.kt
@@ -1,10 +1,19 @@
 package com.alexzh.imbarista.signup
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.alexzh.imbarista.R
 
 class SignUpActivity : AppCompatActivity() {
+
+    companion object {
+
+        fun start(context: Context) {
+            context.startActivity(Intent(context, SignUpActivity::class.java))
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
