a3cbac685f828fda29f5e4ecb7bd680a551d5707
Alex Zhukovich <zhukovich.a@gmail.com>
2019-06-17 22:07:26 +0200

    Added SignUpActivity

---
+++ app/src/main/java/com/alexzh/imbarista/signup/SignUpActivity.kt
@@ -0,0 +1,13 @@
+package com.alexzh.imbarista.signup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.alexzh.imbarista.R
+
+class SignUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
