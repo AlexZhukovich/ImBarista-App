319d3efdc2d6431f65c0fb4ec209e7799e881545
Alex Zhukovich <zhukovich.a@gmail.com>
2019-07-01 20:46:13 +0200

    Added PostExecutionThread interface, CompletableUseCase and SingleUseCase abstract classes as base infrastructure of domain module

---
+++ domain/src/main/java/com/alexzh/imbarista/domain/interactor/CompletableUseCase.kt
@@ -0,0 +1,31 @@
+package com.alexzh.imbarista.domain.interactor
+
+import com.alexzh.imbarista.domain.executor.PostExecutionThread
+import io.reactivex.Completable
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
+import io.reactivex.observers.DisposableCompletableObserver
+import io.reactivex.schedulers.Schedulers
+
+abstract class CompletableUseCase<in Params> constructor(
+    private val postExecutionThread: PostExecutionThread
+) {
+    protected abstract fun buildUseCaseCompletable(params: Params? = null): Completable
+
+    private val disposables = CompositeDisposable()
+
+    open fun execute(observer: DisposableCompletableObserver, params: Params? = null) {
+        val completable = this.buildUseCaseCompletable(params)
+            .subscribeOn(Schedulers.io())
+            .observeOn(postExecutionThread.scheduler)
+        addDisposable(completable.subscribeWith(observer))
+    }
+
+    private fun addDisposable(disposable: Disposable) {
+        disposables.add(disposable)
+    }
+
+    fun dispose() {
+        disposables.dispose()
+    }
+}
---
+++ domain/src/main/java/com/alexzh/imbarista/domain/interactor/SingleUseCase.kt
@@ -0,0 +1,31 @@
+package com.alexzh.imbarista.domain.interactor
+
+import com.alexzh.imbarista.domain.executor.PostExecutionThread
+import io.reactivex.Single
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
+import io.reactivex.observers.DisposableSingleObserver
+import io.reactivex.schedulers.Schedulers
+
+abstract class SingleUseCase<T, in Params> constructor(
+    private val postExecutionThread: PostExecutionThread
+) {
+    abstract fun buildUseCaseObservable(params: Params? = null): Single<T>
+
+    private val disposables = CompositeDisposable()
+
+    open fun execute(singleObserver: DisposableSingleObserver<T>, params: Params? = null) {
+        val single = this.buildUseCaseObservable(params)
+            .subscribeOn(Schedulers.io())
+            .observeOn(postExecutionThread.scheduler)
+        addDisposable(single.subscribeWith(singleObserver))
+    }
+
+    private fun addDisposable(disposable: Disposable) {
+        disposables.add(disposable)
+    }
+
+    fun dispose() {
+        disposables.dispose()
+    }
+}
