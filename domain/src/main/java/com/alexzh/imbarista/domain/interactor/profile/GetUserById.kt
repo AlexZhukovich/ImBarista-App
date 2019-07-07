305eb4e7c5c346028526567f9a15f2a89145ed46
Alex Zhukovich <zhukovich.a@gmail.com>
2019-07-07 20:02:28 +0200

    Added GetUserById use case and tests for it; Added User data class and ProfileRepository#getUser method

---
+++ domain/src/main/java/com/alexzh/imbarista/domain/interactor/profile/GetUserById.kt
@@ -0,0 +1,28 @@
+package com.alexzh.imbarista.domain.interactor.profile
+
+import com.alexzh.imbarista.domain.executor.PostExecutionThread
+import com.alexzh.imbarista.domain.interactor.SingleUseCase
+import com.alexzh.imbarista.domain.model.User
+import com.alexzh.imbarista.domain.repository.ProfileRepository
+import io.reactivex.Single
+import java.lang.IllegalArgumentException
+
+class GetUserById(
+    private val profileRepository: ProfileRepository,
+    postExecutionThread: PostExecutionThread
+) : SingleUseCase<User, GetUserById.Param>(postExecutionThread) {
+
+    override fun buildSingleUseCase(param: Param?): Single<User> {
+        if (param == null) throw IllegalArgumentException("Param can't be null")
+        return profileRepository.getUser(param.userId)
+    }
+
+    data class Param(val userId: Long) {
+
+        companion object {
+            fun forUser(id: Long): Param {
+                return Param(id)
+            }
+        }
+    }
+}
--- domain/src/main/java/com/alexzh/imbarista/domain/repository/ProfileRepository.kt
+++ domain/src/main/java/com/alexzh/imbarista/domain/repository/ProfileRepository.kt
@@ -1,8 +1,12 @@
 package com.alexzh.imbarista.domain.repository
 
+import com.alexzh.imbarista.domain.model.User
 import io.reactivex.Completable
+import io.reactivex.Single
 
 interface ProfileRepository {
+
+    fun getUser(id: Long): Single<User>
 
     fun changeName(newName: String): Completable
 
