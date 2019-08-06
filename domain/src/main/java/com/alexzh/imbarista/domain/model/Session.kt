fd95594a4a7ae69e8e7283056ec3758ec72e21e9
Alex Zhukovich <zhukovich.a@gmail.com>
2019-08-06 23:12:25 +0200

    Updated UserRepository, User class and added Session class in domain module; Updated LogIn use case implementation in domain module

---
+++ domain/src/main/java/com/alexzh/imbarista/domain/model/Session.kt
@@ -0,0 +1,9 @@
+package com.alexzh.imbarista.domain.model
+
+class Session(
+    val sessionId: Long = -1L,
+    val accessToken: String = "",
+    val accessTokenExpiry: Long = -1L,
+    val refreshToken: String = "",
+    val refreshTokenExpiry: Long = -1L
+)
--- domain/src/main/java/com/alexzh/imbarista/domain/repository/UserRepository.kt
+++ domain/src/main/java/com/alexzh/imbarista/domain/repository/UserRepository.kt
@@ -1,15 +1,16 @@
 package com.alexzh.imbarista.domain.repository
 
 import com.alexzh.imbarista.domain.model.AuthUser
+import com.alexzh.imbarista.domain.model.Session
 import com.alexzh.imbarista.domain.model.User
 import io.reactivex.Completable
 import io.reactivex.Single
 
 interface UserRepository {
 
-    fun logIn(email: String, password: String): Single<AuthUser>
-
     fun createAccount(name: String, email: String, password: String): Single<AuthUser>
+
+    fun logIn(email: String, password: String): Single<Session>
 
     fun logOut(): Completable
 
