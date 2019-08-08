be03ac9c7d1c1d92c97ee7238f2b7b24ed8b86d6
Alex Zhukovich <zhukovich.a@gmail.com>
2019-08-08 14:08:30 +0200

    Added extension function isValidEmail to String file; Added error strings to strings.xml file; Updated LoginActivity and LoginViewModel classes

--- mobile-ui/src/main/java/com/alexzh/imbarista/ui/login/LoginActivity.kt
+++ mobile-ui/src/main/java/com/alexzh/imbarista/ui/login/LoginActivity.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
 import androidx.lifecycle.Observer
 import com.alexzh.imbarista.R
@@ -20,6 +22,8 @@ import java.net.UnknownHostException
 class LoginActivity : AppCompatActivity() {
 
     companion object {
+        private const val INVALID_RESOURCE_VALUE = -1
+
         fun start(context: Context) {
             context.startActivity(Intent(context, LoginActivity::class.java))
         }
@@ -43,14 +47,54 @@ class LoginActivity : AppCompatActivity() {
         createAccountButton.setOnClickListener {
             CreateAccountActivity.start(this@LoginActivity)
         }
+
+        emailEditText.addTextChangedListener(object: TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                // not used
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // not used
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                viewModel.changeEmailText(s.toString())
+            }
+        })
+
+        emailEditText.setOnFocusChangeListener{
+                _, hasFocus -> if (hasFocus) viewModel.changeEmailText(emailEditText.text.toString())
+        }
+
+        passwordEditText.addTextChangedListener(object: TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                // not used
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // not used
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                viewModel.changePasswordText(s.toString())
+            }
+        })
+
+        passwordEditText.setOnFocusChangeListener{
+                _, hasFocus -> if (hasFocus) viewModel.changePasswordText(passwordEditText.text.toString())
+        }
     }
 
     override fun onStart() {
         super.onStart()
         viewModel.getLogInInfo().observe(this, Observer<Resource<SessionView>>{
-            it?.let {
-                handleState(it)
-            }
+            it?.let { handleState(it) }
+        })
+        viewModel.getEmailError().observe(this, Observer<Int> {
+            it?.let { emailInputLayout.error = if (it != INVALID_RESOURCE_VALUE) getString(it) else "" }
+        })
+        viewModel.getPasswordError().observe(this, Observer<Int> {
+            it?.let { passwordInputLayout.error = if (it != INVALID_RESOURCE_VALUE) getString(it) else "" }
         })
     }
 
--- mobile-ui/src/main/java/com/alexzh/imbarista/ui/login/LoginViewModel.kt
+++ mobile-ui/src/main/java/com/alexzh/imbarista/ui/login/LoginViewModel.kt
@@ -3,8 +3,10 @@ package com.alexzh.imbarista.ui.login
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.alexzh.imbarista.R
 import com.alexzh.imbarista.domain.interactor.user.LogIn
 import com.alexzh.imbarista.domain.model.Session
+import com.alexzh.imbarista.ext.isValidEmail
 import com.alexzh.imbarista.mapper.SessionViewModelMapper
 import com.alexzh.imbarista.model.SessionView
 import com.alexzh.imbarista.state.Resource
@@ -16,9 +18,19 @@ class LoginViewModel(
     private val mapper: SessionViewModelMapper
 ): ViewModel() {
 
+    companion object {
+        private const val INVALID_RESOURCE_VALUE = -1
+    }
+
     private val liveData: MutableLiveData<Resource<SessionView>> = MutableLiveData()
+    private val emailErrorLiveData: MutableLiveData<Int> = MutableLiveData()
+    private val passwordErrorLiveData: MutableLiveData<Int> = MutableLiveData()
 
     fun getLogInInfo(): LiveData<Resource<SessionView>> = liveData
+
+    fun getEmailError(): LiveData<Int> = emailErrorLiveData
+
+    fun getPasswordError(): LiveData<Int> = passwordErrorLiveData
 
     fun logIn(email: String, password: String) {
         liveData.postValue(Resource(ResourceState.LOADING, null, null))
@@ -32,6 +44,22 @@ class LoginViewModel(
     override fun onCleared() {
         logIn.dispose()
         super.onCleared()
+    }
+
+    fun changeEmailText(email: String) {
+        when {
+            email.isBlank() -> emailErrorLiveData.postValue(R.string.error_email_is_blank)
+            email.isValidEmail() -> emailErrorLiveData.postValue(R.string.error_email_is_not_email)
+            else -> emailErrorLiveData.postValue(INVALID_RESOURCE_VALUE)
+        }
+    }
+
+    fun changePasswordText(password: String) {
+        if (password.isBlank()) {
+            passwordErrorLiveData.postValue(R.string.error_password_is_blank)
+        }  else {
+            passwordErrorLiveData.postValue(INVALID_RESOURCE_VALUE)
+        }
     }
 
     private inner class LogInSubscriber: DisposableSingleObserver<Session>() {
