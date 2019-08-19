c333869d89d5e8ae04853e971e5e46013afa88eb
Alex Zhukovich <zhukovich.a@gmail.com>
2019-08-19 16:54:15 +0200

    Fixed issue with overriding search radius and number cafes on map values

--- mobile-ui/src/main/java/com/alexzh/imbarista/ImBaristaApp.kt
+++ mobile-ui/src/main/java/com/alexzh/imbarista/ImBaristaApp.kt
@@ -8,6 +8,7 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 import org.koin.core.logger.Level
+import java.lang.NumberFormatException
 
 class ImBaristaApp : Application() {
 
@@ -20,11 +21,15 @@ class ImBaristaApp : Application() {
     }
 
     private fun initDefaultValues() {
-        val searchRadius = getString(R.string.pref_cafe_search_radius_default_value).toInt()
-        val numberCafesOnMap = getString(R.string.pref_cafe_search_cafe_limits_on_map_default_value).toInt()
-
-        preferencesRepository.saveDefaultSearchRadius(searchRadius)
-        preferencesRepository.saveDefaultNumberCafesOnMap(numberCafesOnMap)
+        try {
+            preferencesRepository.getSearchRadius()
+            preferencesRepository.getNumberCafesOnMap()
+        } catch (ex: NumberFormatException) {
+            val defaultSearchRadius = getString(R.string.pref_cafe_search_radius_default_value).toInt()
+            val defaultNumberCafesOnMap = getString(R.string.pref_cafe_search_cafe_limits_on_map_default_value).toInt()
+            preferencesRepository.saveDefaultSearchRadius(defaultSearchRadius)
+            preferencesRepository.saveDefaultNumberCafesOnMap(defaultNumberCafesOnMap)
+        }
     }
 
     private fun initDI() {
